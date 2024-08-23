e280d2b0baed9ba0947301f6f037f9deb3dd51ee
Mike Saburenkov <mikesub@users.noreply.github.com>
2024-08-23 22:15:53 +0200

    update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,16 @@
-import image from '@rollup/plugin-image';
-import sucrase from '@rollup/plugin-sucrase';
-import css from 'rollup-plugin-css-only';
+import image from "@rollup/plugin-image";
+import sucrase from "@rollup/plugin-sucrase";
+import css from "rollup-plugin-css-only";
 
 export default {
-	input: 'src/index.js',
-	output: [
-		{ file: './dist/index.js', format: 'es' }
-	],
+	input: "src/index.js",
+	output: [{ file: "./dist/uikit.js", format: "es" }],
 	plugins: [
-css({output:'index.css'}),
-
-	  sucrase({
-      exclude: ['node_modules/**'],
-      transforms: ['jsx']
-    }),
-    image()
-	]
-}
+		css({ output: "uikit.css" }),
+		sucrase({
+			exclude: ["node_modules/**"],
+			transforms: ["jsx"],
+		}),
+		image(),
+	],
+};
